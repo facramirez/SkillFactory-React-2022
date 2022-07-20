b39de4137097ab822874dadefe90dc3c52c8a203
Facundo Ramirez <88752866+facramirez@users.noreply.github.com>
2022-07-20 12:36:23 -0300

    Update index.js

--- Practica JS/index.js
+++ Practica JS/index.js
@@ -1,15 +1,19 @@
-let name = 'Facundo'
+let userName = 'Facundo'
 let age = '29'
-let isCouple = True
+let isCouple = true
 let numerosArray = [1,2,3,4,5,6,7,8,9,10]
-let nombresSkillers = [
-    'Fabricio',
-    'Matias',
-    'Melina',
-    'Adan',
-    'Sofia'
-]
+let nombresSkillers = ['Fabricio','Matias','Melina','Adan','Sofia']
 
 const objeto = {
-    let nombre = 'Facundo',
+    userName: 'Facundo',
+    age: 29,
+    isCouple: true,
+    numerosArray: [1,2,3,4,5,6,7,8,9,10],
+    nombresSkillers: ['Fabricio','Matias','Melina','Adan','Sofia']
 }
+
+console.log(userName)
+console.log(age)
+console.log(isCouple)
+console.log(numerosArray)
+console.log(objeto)
