416b595d8f0680bc642cd07e49422a697af089cd
Facundo Ramirez <88752866+facramirez@users.noreply.github.com>
2022-07-20 09:58:56 -0300

    JS

---
+++ Practica JS/index.js
@@ -0,0 +1,15 @@
+let name = 'Facundo'
+let age = '29'
+let isCouple = True
+let numerosArray = [1,2,3,4,5,6,7,8,9,10]
+let nombresSkillers = [
+    'Fabricio',
+    'Matias',
+    'Melina',
+    'Adan',
+    'Sofia'
+]
+
+const objeto = {
+    let nombre = 'Facundo',
+}
