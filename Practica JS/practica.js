90388abaecffc2d6c18bc779eaaf15f3b3e236c4
Facundo Ramirez <88752866+facramirez@users.noreply.github.com>
2022-07-31 23:10:54 -0300

    Update

--- Practica JS/practica.js
+++ Practica JS/practica.js
@@ -1,2 +1,10 @@
 const primerNumero = prompt('Ingrese el primer numero: ')
 const segundoNumero = prompt('Ingrese el segundo numero: ')
+
+if(typeof primerNumero != )
+
+
+
+
+
+const primerNumero = prompt('Ingrese el primer )
